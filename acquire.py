13fdde8e4dcc07d83fe56f97c454593880369845
Veronica F Reyes <veronica.fierro.reyes1@gmail.com>
2021-08-26 11:51:46 -0500

    Adding Initial Project Notebook

---
+++ acquire.py
@@ -0,0 +1,55 @@
+ 
+# Functions to obtain Telco Customer data from the Codeup Data Science Database: telco_churn
+#It returns a pandas dataframe.
+#--------------------------------
+
+#This function uses my info from my env file to create a connection url to access the Codeup db.  
+
+from typing import Container
+import pandas as pd
+import os
+from env import host, user, password
+
+#Function to connect to database for SQL query use
+def get_db_url(host, user, password, database):
+        
+    url = f'mysql+pymysql://{user}:{password}@{host}/{database}'
+    
+    return url
+
+
+#Function to get data from telco_churn database
+def get_telco_data():
+    
+    filename = "telco_churn.csv"
+
+    if os.path.isfile(filename):
+        return pd.read_csv(filename)
+    else:
+
+        database = 'telco_churn'
+
+        #Create SQL query to select data from telco_churn database
+        query = '''
+                SELECT * FROM customers
+                JOIN contract_types ON customers.contract_type_id = contract_types.contract_type_id
+                JOIN internet_service_types ON customers.internet_service_type_id = internet_service_types.internet_service_type_id
+                JOIN payment_types ON customers.payment_type_id = payment_types.payment_type_id;
+                '''
+
+         # read the SQL query into a dataframe
+        df = pd.read_sql(query, get_db_url(host,user, password, database))
+
+         # Write that dataframe to disk for later. Called "caching" the data for later.
+        df.to_csv(filename)
+
+        # Return the dataframe to the calling code
+        return df
+
+#Call function to get and create telco_churn.csv locally
+get_telco_data()
+
+
+
+
+
---
+++ prepare.py
@@ -0,0 +1,132 @@
+#Prepare Telco data using various functions to clean up and section data off 
+
+
+import pandas as pd
+import numpy as np
+
+from sklearn.model_selection import train_test_split
+from sklearn.impute import SimpleImputer
+
+###################### Prepare Iris Data ######################
+
+def prep_telco(df):
+
+    '''Prepares acquired Telco data for exploration'''
+    
+    # drop column in case it is created in addition to the index
+    df =  df. df_telco.drop(columns=['Unnamed: 0'])
+
+    # drop column using .drop(columns=column_name)
+    df = df.drop(columns='customer_id')
+    
+    # remame column using .rename(columns={current_column_name : replacement_column_name})
+    #df = df.rename(columns={'col_name':'column'})
+    
+    # create dummies dataframe using .get_dummies(column_name,not dropping any of the dummy columns)
+    dummy_df = pd.get_dummies(df['churn'], drop_first=False)
+    
+    # join original df with dummies df using .concat([original_df,dummy_df], join along the index)
+    df = pd.concat([df, dummy_df], axis=1)
+    
+    return df
+
+###################### Prepare Telco Data With Split ######################
+
+def split_data(df):
+    '''
+    take in a DataFrame and return train, validate, and test DataFrames; stratify on churn.
+    return train, validate, test DataFrames.
+    '''
+    
+    # splits df into train_validate and test using train_test_split() stratifying on churn to get an even mix 
+    train_validate, test = train_test_split(df, test_size=.2, random_state=123, stratify=df.churn)
+    
+    # splits train_validate into train and validate using train_test_split() stratifying on churn to get an even mix
+    train, validate = train_test_split(train_validate, 
+                                       test_size=.3, 
+                                       random_state=123, 
+                                       stratify=train_validate.churn)
+    return train, validate, test
+
+
+def prep_iris_with_split(df):
+    '''Prepares acquired Iris data for exploration'''
+    
+    # drop column using .drop(columns=column_name)
+    df = df.drop(columns='species_id')
+    
+    # remame column using .rename(columns={current_column_name : replacement_column_name})
+    df = df.rename(columns={'species_name':'species'})
+    
+    # create dummies dataframe using .get_dummies(column_name,not dropping any of the dummy columns)
+    dummy_df = pd.get_dummies(df['species'], drop_first=False)
+    
+    # join original df with dummies df using .concat([original_df,dummy_df], join along the index)
+    df = pd.concat([df, dummy_df], axis=1)
+    
+    # split data into train/validate/test using split_data function
+    train, validate, test = split_data(df)
+    
+    return train, validate, test
+
+def prep_titanic(df):
+    '''
+    This function take in the titanic data acquired by get_titanic_data,
+    Returns prepped train, validate, and test dfs with embarked dummy vars,
+    deck dropped, and the mean of age imputed for Null values.
+    '''
+    
+    # drop rows where embarked/embark town are null values
+    df = df[~df.embarked.isnull()]
+    
+    # encode embarked using dummy columns
+    titanic_dummies = pd.get_dummies(df.embarked, drop_first=True)
+    
+    # join dummy columns back to df
+    df = pd.concat([df, titanic_dummies], axis=1)
+    
+    # drop the deck column
+    df = df.drop(columns='deck')
+    
+    # split data into train, validate, test dfs
+    train, validate, test = titanic_split(df)
+    
+    # impute mean of age into null values in age column
+    train, validate, test = impute_mean_age(train, validate, test)
+    
+    return train, validate, test
+
+def titanic_split(df):
+    '''
+    This function take in the titanic data acquired by get_titanic_data,
+    performs a split and stratifies survived column.
+    Returns train, validate, and test dfs.
+    '''
+    train_validate, test = train_test_split(df, test_size=.2, 
+                                        random_state=123, 
+                                        stratify=df.survived)
+    train, validate = train_test_split(train_validate, test_size=.3, 
+                                   random_state=123, 
+                                   stratify=train_validate.survived)
+    return train, validate, test
+
+
+def impute_mean_age(train, validate, test):
+    '''
+    This function imputes the mean of the age column for
+    observations with missing values.
+    Returns transformed train, validate, and test df.
+    '''
+    # create the imputer object with mean strategy
+    imputer = SimpleImputer(strategy = 'mean')
+    
+    # fit on and transform age column in train
+    train['age'] = imputer.fit_transform(train[['age']])
+    
+    # transform age column in validate
+    validate['age'] = imputer.transform(validate[['age']])
+    
+    # transform age column in test
+    test['age'] = imputer.transform(test[['age']])
+    
+    return train, validate, test
